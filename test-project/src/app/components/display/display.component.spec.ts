e8531f1fc02dc608ecf5b3917df8419b9f5b9eb9
Alin-Apinzarasoaie <alinapinzarasoaie@gmail.com>
2024-06-29 11:55:15 +0300

    Lint fixing project.

--- test-project/src/app/components/display/display.component.spec.ts
+++ test-project/src/app/components/display/display.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DisplayComponent } from './display.component';
 
 describe('DisplayComponent', () => {
-  let component: DisplayComponent;
-  let fixture: ComponentFixture<DisplayComponent>;
+    let component: DisplayComponent;
+    let fixture: ComponentFixture<DisplayComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DisplayComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DisplayComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DisplayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DisplayComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
