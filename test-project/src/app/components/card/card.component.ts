b944f5fba84d32379fe9cd2f9920db8799068550
Alin-Apinzarasoaie <alinapinzarasoaie@gmail.com>
2024-06-29 21:58:46 +0300

    Styled card and display components according to Figma's design.

--- test-project/src/app/components/card/card.component.ts
+++ test-project/src/app/components/card/card.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {}
+export class CardComponent {
+    @Input()
+    public disabled: boolean = false;
+
+    @Input()
+    public title: string = 'Title';
+
+    @Input()
+    public subtitle: string = 'Subtitle';
+
+    public active: boolean = false;
+
+    public theme: 'blue' | 'green' | 'yellow' | 'red' | 'orange' = 'orange';
+
+    public onCardClicked(): void {
+        if (this.disabled) {
+            return;
+        }
+
+        this.active = !this.active;
+    }
+
+    public onCloseClicked(): void {
+        this.active = false;
+    }
+}
